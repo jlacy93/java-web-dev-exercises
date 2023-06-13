0c0800dc8354267836b7a8f6dac0bccfa50c5030
jlacy <josephinalacy@gmail.com>
2023-06-13 10:42:04 -0500

    Lesson 3 classes

---
+++ src/org/launchcode/java/demos/lsn3classes1/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+public class HelloWorld {
+    public String message = "Hello World";
+
+    public void sayHello(){
+        System.out.println(message);
+    }
+}
